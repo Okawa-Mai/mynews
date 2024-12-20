7f274c2bbdf939ab842566b2296087de85247306
EC2 Default User <ec2-user@ip-172-31-33-153.ap-northeast-1.compute.internal>
2024-12-20 12:12:35 +0000

    Laravel 14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,26 +5,32 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//以下の1行を追記することで、profile Modelが扱えるようになる
+use App\Models\Profile;
+
+
 class ProfileController extends Controller
 {
-    //以下を追記
     public function add()
     {
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        // Validationを行う
+        $this->validate($request, Profile::$rules);
+
+        $pofile = new Pofile;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+
         return redirect('admin/profile/create');
-    }
-
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-
-    public function update()
-    {
-        return redirect('admin/profile/edit');
     }
 }
